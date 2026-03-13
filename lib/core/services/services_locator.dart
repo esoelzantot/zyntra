5f093b32f792ef1a14ffd25732aa191af3ef56a4
EsoElzantot <eslamelzantot@gmail.com>
2026-03-14 00:53:02 +0200

    handle dependancy_injection in service_locator

--- lib/core/services/services_locator.dart
+++ lib/core/services/services_locator.dart
@@ -2,10 +2,21 @@ import 'package:get_it/get_it.dart';
 import 'package:zyntra/core/apis/api_services.dart';
 import 'package:zyntra/core/cache/articles_cache_service_factory.dart';
 import 'package:zyntra/core/cache/threads_cache_service_factory.dart';
+import 'package:zyntra/features/article_details/data/data_sources/local/article_details_local_data_source_impl.dart';
+import 'package:zyntra/features/article_details/data/data_sources/remote/article_details_remote_data_source_impl.dart';
+import 'package:zyntra/features/article_details/data/repos/article_details_repo_impl.dart';
+import 'package:zyntra/features/article_details/domain/use_cases/get_article_analysis_use_case.dart';
+import 'package:zyntra/features/article_details/domain/use_cases/get_article_data_use_case.dart';
+import 'package:zyntra/features/article_details/domain/use_cases/get_article_mindmap_use_case.dart';
 import 'package:zyntra/features/articles/data/data_sources/local/articles_local_data_source_impl.dart';
 import 'package:zyntra/features/articles/data/data_sources/remote/articles_remote_data_source_impl.dart';
 import 'package:zyntra/features/articles/data/repos/articles_repo_impl.dart';
 import 'package:zyntra/features/articles/domain/use_cases/get_all_articles_use_case.dart';
+import 'package:zyntra/features/asky_ai/data/data_sources/local/asky_local_data_source_impl.dart';
+import 'package:zyntra/features/asky_ai/data/data_sources/remote/asky_remote_data_source_impl.dart';
+import 'package:zyntra/features/asky_ai/data/repos/asky_repo_impl.dart';
+import 'package:zyntra/features/asky_ai/domain/use_cases/get_all_threads_use_case.dart';
+import 'package:zyntra/features/asky_ai/domain/use_cases/send_query_use_case.dart';
 import 'package:zyntra/features/home/data/data_sources/local/home_local_data_source_impl.dart';
 import 'package:zyntra/features/home/data/data_sources/remote/home_remote_data_source_impl.dart';
 import 'package:zyntra/features/home/data/repos/home_repo_impl.dart';
@@ -88,5 +99,65 @@ void setupServiceLocator() {
 
   getIt.registerSingleton<GetSavedArticlesUseCase>(
     GetSavedArticlesUseCase(repo: getIt.get<LibraryRepoImpl>()),
+  );
+
+  // ARTICLE DETAILS FEATURE
+  getIt.registerSingleton<ArticleDetailsLocalDataSourceImpl>(
+    ArticleDetailsLocalDataSourceImpl(
+      cacheFactory: getIt<ArticlesCacheServiceFactory>(),
+    ),
+  );
+
+  getIt.registerSingleton<ArticleDetailsRemoteDataSourceImpl>(
+    ArticleDetailsRemoteDataSourceImpl(
+      api: getIt<ApiServices>(),
+      cacheFactory: getIt<ArticlesCacheServiceFactory>(),
+    ),
+  );
+
+  getIt.registerSingleton<ArticleDetailsRepoImpl>(
+    ArticleDetailsRepoImpl(
+      local: getIt.get<ArticleDetailsLocalDataSourceImpl>(),
+      remote: getIt.get<ArticleDetailsRemoteDataSourceImpl>(),
+    ),
+  );
+
+  getIt.registerSingleton<GetArticleDataUseCase>(
+    GetArticleDataUseCase(repo: getIt.get<ArticleDetailsRepoImpl>()),
+  );
+
+  getIt.registerSingleton<GetArticleAnalysisUseCase>(
+    GetArticleAnalysisUseCase(repo: getIt.get<ArticleDetailsRepoImpl>()),
+  );
+
+  getIt.registerSingleton<GetArticleMindmapUseCase>(
+    GetArticleMindmapUseCase(repo: getIt.get<ArticleDetailsRepoImpl>()),
+  );
+
+  // ASKY FEATURE
+  getIt.registerSingleton<AskyLocalDataSourceImpl>(
+    AskyLocalDataSourceImpl(cacheFactory: getIt<ThreadsCacheServiceFactory>()),
+  );
+
+  getIt.registerSingleton<AskyRemoteDataSourceImpl>(
+    AskyRemoteDataSourceImpl(
+      api: getIt<ApiServices>(),
+      cacheFactory: getIt<ThreadsCacheServiceFactory>(),
+    ),
+  );
+
+  getIt.registerSingleton<AskyRepoImpl>(
+    AskyRepoImpl(
+      local: getIt.get<AskyLocalDataSourceImpl>(),
+      remote: getIt.get<AskyRemoteDataSourceImpl>(),
+    ),
+  );
+
+  getIt.registerSingleton<GetAllThreadsUseCase>(
+    GetAllThreadsUseCase(repo: getIt.get<AskyRepoImpl>()),
+  );
+
+  getIt.registerSingleton<SendQueryUseCase>(
+    SendQueryUseCase(repo: getIt.get<AskyRepoImpl>()),
   );
 }
--- lib/features/article_details/domain/use_cases/get_article_mindmap_use_case.dart
+++ lib/features/article_details/domain/use_cases/get_article_mindmap_use_case.dart
@@ -3,10 +3,10 @@ import 'package:zyntra/core/errors/failure.dart';
 import 'package:zyntra/core/services/use_case.dart';
 import 'package:zyntra/features/article_details/domain/repos/article_details_repo.dart';
 
-class CreateArticleMindmapUseCase extends UseCase<String, String> {
+class GetArticleMindmapUseCase extends UseCase<String, String> {
   final ArticleDetailsRepo repo;
 
-  CreateArticleMindmapUseCase({required this.repo});
+  GetArticleMindmapUseCase({required this.repo});
 
   @override
   Future<Either<Failure, String>> call([String id = '']) async {
